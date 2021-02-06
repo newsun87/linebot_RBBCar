8b7b9df5a0085132af8d3b6bfe62a82775e07b52
tinaliou4 <tinaliou4@gmail.com>
2021-02-05 18:29:40 -0800

    init.

--- linebot_RBB.py
+++ linebot_RBB.py
@@ -60,8 +60,9 @@ def control():
   else:        
     receive_json_obj = request.get_json() # 取得 json 資料物件 
     ctrl_msg = receive_json_obj['ctrl']
-    print('ctrl_msg', ctrl_msg)    
-    client.publish("RBBCar/control/" + RBBCar_num, ctrl_msg, qos=1)
+    print('ctrl_msg', ctrl_msg)
+    print('RBBCar_num',  + RBBCar_num)    
+    client.publish("RBBCar/control/RBBCar0001", ctrl_msg, qos=1)
     return ctrl_msg # 回傳 json 資料字串
  
 @app.route('/register',methods=['GET','POST'])    
