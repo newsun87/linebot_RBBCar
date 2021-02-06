f11b47c42f4c9d6f12d2522396905e8f9a8b5bc0
tinaliou4 <tinaliou4@gmail.com>
2021-02-05 18:18:34 -0800

    init.

--- linebot_RBB.py
+++ linebot_RBB.py
@@ -45,6 +45,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])    
 def control():
   global userid
+  time.sleep(1)
   print('userid', userid)
   RBBCarath = os.path.dirname(os.path.realpath(__file__))
   cfgpath = os.path.join(RBBCarath, 'linebot_RBBCar.conf')
@@ -59,13 +60,14 @@ def control():
     receive_json_obj = request.get_json() # 取得 json 資料物件 
     ctrl_msg = receive_json_obj['ctrl']
     print('ctrl_msg', ctrl_msg)
-    #RBBCar_num = config.get('device', userid) 
-    client.publish("RBBCar/control/RBBCar0001", ctrl_msg, qos=1)
+    RBBCar_num = config.get('device', userid) 
+    client.publish("RBBCar/control/" + RBBCar_num, ctrl_msg, qos=1)
     return ctrl_msg # 回傳 json 資料字串
  
 @app.route('/register',methods=['GET','POST'])    
 def register(): 
    global userid
+   time.sleep(1)
    RBBCarpath = os.path.dirname(os.path.realpath(__file__))
    cfgpath = os.path.join(RBBCarpath, 'linebot_RBBCar.conf')
    # 創建對象
